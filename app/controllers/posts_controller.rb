5176a6e3d1bb5e323160ce8f057776af9a0f5c28
marta fuste <marta.fustelazcano@gmail.com>
2020-06-23 16:30:37 +0200

    post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+  def edit
+
+  end
+
+  def new
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    authorize @post
+    @post.user = current_user
+    if @post.save
+      redirect_to root_path
+      # GUYS WE NEED TO CHANGE THE REDIRECT ONCE WE HAVE THE FEED
+    else
+      render :new
+    end
+  end
+
+  def delete
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:require).permit(:description, :title, photos: [])
+  end
+end
