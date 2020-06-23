d6845be8cf170ffbf4e1e676c4ad205e594ae69d
marta fuste <marta.fustelazcano@gmail.com>
2020-06-23 13:13:20 +0200

    debug

--- app/policies/contest_policy.rb
+++ app/policies/contest_policy.rb
@@ -1,4 +1,4 @@
-class PostPolicy < ApplicationPolicy
+class ContestPolicy < ApplicationPolicy
   class Scope < Scope #the class scope and then the method resolve is actually the method that authorize all the instances of my clss Restaurant to be authorized
     def resolve
       scope.all #scope corresponds to the Restaurant class
