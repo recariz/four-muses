a99d507bccab06f57dfde38c5791b4fc72b54e56
marta fuste <marta.fustelazcano@gmail.com>
2020-06-24 14:44:32 +0200

    post profile and policies

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,11 @@ Rails.application.routes.draw do
 
   resources :contest_applications, only: [:index, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # the below is for setting up the following action in the controller
+  resources :users, only: [:show] do
+    member do
+      post :follow
+      post :unfollow
+    end
+ end
 end
