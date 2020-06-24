f37a93a708e805a2440d018805010ee02a7267e1
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-06-24 12:19:14 +0200

    seeds improoved

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,7 +2,8 @@ class Contest < ApplicationRecord
   belongs_to :user
   has_many :contest_tags, dependent: :destroy
   has_many :contest_applications, dependent: :destroy
-  validates :start_date, :end_date, :location, :title, :content, presence: true
+  validates :start_date, :end_date, :location, :title, :content, :photo, presence: true
   geocoded_by :location
+  has_one_attached :photo
   after_validation :geocode, if: :will_save_change_to_location?
 end
