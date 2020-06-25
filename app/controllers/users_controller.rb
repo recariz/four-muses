92af2a5f912a842b472aaec7dfe869ca7cc2d8c7
Patricia Recarte <patriciarecarte@hotmail.com>
2020-06-25 19:21:45 +0200

    work in progress for making follows dynamic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,57 +2,69 @@ class UsersController < ApplicationController
  skip_before_action :authenticate_user!, only: :index
  before_action :set_user, only: [:follow, :unfollow, :create, :edit, :show]
 
- def show
-  @photos = []
-  @user.posts.each do |post|
-    post.photos.each do |photo|
-      @photos << photo
+  def show
+    @photos = []
+    @user.posts.each do |post|
+      post.photos.each do |photo|
+        @photos << photo
+      end
     end
   end
- end
 
-def create
-  if !current_user
-    redirect_to user_path(@user)
-  else
+  def create
+    if !current_user
+      redirect_to user_path(@user)
+    else
+
+    end
+  end
+
+  def edit
 
   end
-end
-def edit
 
-end
+  def update
 
-def update
+  end
 
-end
+  def follow
+    p params
+    current_user.follow(params[:user_id])
+    respond_to do |format|
+      format.html { redirect_to user_path(@user) }
+      format.js {
+        @post_id = params[:post_id]
+        @user_id = params[:user_id]
+        @user = User.find(params[:user_id])
+        @posts = @user.posts
+        @post = @posts.first
+        @followings = current_user.followings
+        @my_followed_posts = @posts.select {|post| @followings.include?(post.user)}
+      }
+    end
+  end
 
- def follow
-   if current_user.follow(@user.id)
-     respond_to do |format|
-       format.html { redirect_to user_path(@user) }
-       format.js
-     end
-   end
- end
+  def unfollow
+    if current_user.unfollow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to user_path(@user) }
+        format.js
+      # { render action: :follow }
+      end
+    end
+  end
 
- def unfollow
-
-   if current_user.unfollow(@user.id)
-     respond_to do |format|
-       format.html { redirect_to user_path(@user) }
-       format.js { render action: :follow }
-     end
-   end
- end
+  def feed_card
+    @post = Post.find(params[:id])
+  end
 
  private
 
- def set_user
-  @user = User.find(params[:id])
-  authorize @user
- end
- def strong_params
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
 
- end
+  def strong_params
+  end
 end
-
--- app/javascript/components/toggle.js
+++ app/javascript/components/toggle.js
@@ -1,15 +1,14 @@
-const toggle = document.getElementById('toggle-section');
-const feedGeneral = document.getElementById('feed-general');
-const feedFollows = document.getElementById('feed-follows');
-const toggleBall = document.getElementById('ball');
 
 const toggleSwitch = () => {
+  const toggle = document.getElementById('toggle-section');
+  const feedGeneral = document.getElementById('feed-general');
+  const feedFollows = document.getElementById('feed-follows');
+  const toggleBall = document.getElementById('ball');
   if (toggle) {
     toggle.addEventListener('click', (event) => {
       feedFollows.classList.toggle('active');
       feedGeneral.classList.toggle('active');
       toggleBall.classList.toggle('active');
-      window.scrollTo(0,0);
       // $(window).load(function(){
       //  $(window).scrollTop(0);
       // });
