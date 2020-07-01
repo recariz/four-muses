e3908a9cfc811fc73fee80bc06102aee3925b11b
Patricia Recarte <patriciarecarte@hotmail.com>
2020-07-01 11:59:37 +0200

    added favicon and changed contest index

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@ require 'faker'
 require 'open-uri'
 
 
-# p "Deleting likes"
-#  Vote.delete_all
 
-
+p "Deleting messages"
+Message.delete_all
+p "Deleting chatrooms"
+Chatroom.delete_all
 p "Deleting comments"
 Comment.delete_all
 p "Deleting Contest Applications"
